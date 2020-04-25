1addf91f1ed7fd99b32e65797adf5a3688dedf56
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-04-25 09:19:42 +0300

    Update login.jsx

--- ui/src/dashboard/login.jsx
+++ ui/src/dashboard/login.jsx
@@ -71,7 +71,7 @@ export default function Login() {
                     <LockOutlinedIcon />
                 </Avatar>
                 <Typography component="h1" variant="h5">
-                    Startblock
+                    Chain++
                 </Typography>
                 <form className={classes.form} noValidate>
                     <TextField
